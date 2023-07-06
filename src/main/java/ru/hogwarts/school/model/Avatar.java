4f1f8f8cfe92e758ed107c16dbaa470516df1af4
semenikhin <garry@nextmail.ru>
2023-07-06 23:18:39 +0300

    Second commit

---
+++ src/main/java/ru/hogwarts/school/model/Avatar.java
@@ -0,0 +1,32 @@
+package ru.hogwarts.school.model;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(exclude = "student")
+@ToString(exclude = "student")
+@Builder
+@Entity
+public class Avatar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private long id;
+
+    private String filePath;
+
+    @Column(nullable = false)
+    private long fileSize;
+
+    private String mediaType;
+
+    private byte[] data;
+
+    @OneToOne
+    private Student student;
+}
